ddd66b051ecd6ce25dc0ff478c6c82fc3e06a104
unknown <david.19070008@itsmotul.edu.mx>
2023-02-16 17:07:56 -0600

    tarea

---
+++ src/App.vue
@@ -0,0 +1,309 @@
+<script setup>
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+<div id="container" class="bg-blue-200">
+  <div class="grid grid-cols-3 gap-4">
+  <div class="..."></div>
+  <div class="items-center">
+      <svg width="100%" height="100%" viewBox="0 0 500 150" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule: evenodd; clip-rule: evenodd; stroke-miterlimit: 1.5;"><rect id="Mesa-de-trabajo4" serif:id="Mesa de trabajo4" x="0" y="0" width="500" height="150" style="fill: none;"></rect><g><g><g><g id="Capa2"><path d="M82.775,89.986l-9.562,-15.753l-18.702,-0.24c0,0 9.563,15.754 9.563,15.754l18.701,0.239Z" style="fill:none;stroke:#023047;stroke-width:6px;"></path><path d="M45.378,89.504l9.133,-15.511c0,0 9.563,15.755 9.563,15.754c0,0.001 -9.135,15.509 -9.135,15.509l-9.561,-15.752Z" style="fill:none;stroke:#023047;stroke-width:6px;"></path><path d="M64.074,89.747l-9.135,15.509l18.697,0.245l9.139,-15.515l-18.701,-0.239Z" style="fill:none;stroke:#023047;stroke-width:6px;"></path></g><path d="M71.072,58.44c-3.555,-1.955 -4.912,0.592 -8.244,0.633l-1.686,-5.366c-4.894,0.247 -9.664,1.424 -14.038,3.463l1.445,5.426c-2.909,1.498 -5.543,3.435 -7.807,5.74l-5.093,-2.744c-3.188,3.521 -5.61,7.633 -7.126,12.099l4.97,2.951c-0.905,3.074 -1.299,6.279 -1.168,9.497l-5.517,1.486c0.385,4.733 1.731,9.37 3.96,13.648l5.584,-1.253c1.629,2.849 3.706,5.445 6.155,7.691l-2.709,4.845c3.733,3.172 8.057,5.619 12.726,7.202l2.927,-4.722c3.209,0.955 6.541,1.42 9.872,1.379l1.686,5.366c4.894,-0.247 9.664,-1.423 14.038,-3.463l-1.445,-5.425c2.909,-1.498 5.544,-3.436 7.807,-5.741l5.093,2.744c3.188,-3.521 5.61,-7.632 7.127,-12.099l-4.971,-2.951c0.905,-3.074 1.299,-6.278 1.168,-9.497l5.517,-1.486c-0.385,-4.732 -1.731,-9.37 -3.959,-13.647l-5.585,1.252c-1.629,-2.849 -0.712,-4.696 -3.383,-6.649" style="fill:none;stroke:#023047;stroke-width:6px;stroke-linecap:square;"></path></g><path d="M69.034,61.836l19.288,-32.745l-8.61,-4.872" style="fill:none;stroke:#023047;stroke-width:6px;"></path><path d="M86.255,71.582l19.289,-32.745l8.61,4.872" style="fill:none;stroke:#023047;stroke-width:6px;"></path></g><path d="M132.38,113.192l-0,-49.971l7.193,-0l-0,49.971l-7.193,0Zm6.953,-49.731l-6.713,-0l-0,49.491l6.713,0l0,-49.491Zm-6.953,-0.24l-0,49.971l7.193,0l-0,-49.971l-7.193,-0Z" style="fill:#023047;fill-rule:nonzero;"></path><path d="M180.545,84.651c-0,-0 -14.296,-0 -14.296,-0c-0.409,-0.035 -0.823,-0.055 -1.241,-0.055c-0.243,-0 -0.484,0.008 -0.725,0.02c-3.574,-0.354 -6.374,-3.377 -6.374,-7.043c-0,-3.903 3.174,-7.078 7.078,-7.078l15.8,0c3.666,0.132 6.627,3.052 6.828,6.702l7.2,-0c-0.067,-2.436 -0.743,-4.719 -1.877,-6.702c-0.002,-0.002 -0.002,-0.005 -0.005,-0.007c-2.409,-4.196 -6.883,-7.056 -12.038,-7.186l-15.908,0c-5.306,0 -9.936,2.897 -12.396,7.193c-1.194,2.086 -1.875,4.503 -1.875,7.078c0,2.575 0.681,4.992 1.875,7.078c0.599,1.045 1.326,2.007 2.16,2.865c2.592,2.668 6.22,4.328 10.236,4.328l15.558,-0c3.903,-0 7.078,3.174 7.078,7.077c-0,3.904 -3.175,7.078 -7.078,7.078l-15.537,0c-3.786,0 -6.886,-2.973 -7.092,-6.708l-7.2,-0c0.067,2.438 0.746,4.723 1.882,6.708c2.467,4.297 7.099,7.193 12.41,7.193l15.537,0c5.306,0 9.935,-2.896 12.395,-7.193c1.194,-2.086 1.875,-4.503 1.875,-7.078c0,-2.575 -0.681,-4.991 -1.875,-7.077c-2.46,-4.297 -7.089,-7.193 -12.395,-7.193Z" style="fill:#023047;fill-rule:nonzero;"></path><path d="M241.308,105.999c-8.576,0 -15.755,-6.083 -17.452,-14.163c-0.247,-1.184 -0.379,-2.412 -0.379,-3.668c-0,-1.206 0.12,-2.386 0.35,-3.524c1.64,-8.15 8.854,-14.307 17.481,-14.307c7.363,0 13.697,4.486 16.414,10.868l7.624,0c-3.011,-10.432 -12.635,-18.061 -24.038,-18.061c-12.624,0 -23.065,9.348 -24.775,21.5c-0.165,1.15 -0.249,2.328 -0.249,3.524c-0,1.247 0.091,2.472 0.268,3.668c1.775,12.082 12.183,21.356 24.756,21.356c11.12,0 20.547,-7.255 23.803,-17.289l-7.739,-0c-2.884,5.97 -9.001,10.096 -16.064,10.096Z" style="fill:#023047;fill-rule:nonzero;"></path><path d="M295.381,105.999l-0,-43.063l-7.193,-0l-0,50.256l38.805,0l0,-7.193l-31.612,0Z" style="fill:#023047;fill-rule:nonzero;"></path><path d="M342.272,113.192l9.502,0l15.817,-28.582l-4.726,-8.622l-20.593,37.204Zm32.452,-41.464l-4.752,-8.584l-0.003,0.005l-4.747,8.579l22.948,41.464l9.504,0l-22.95,-41.464Z" style="fill:#023047;fill-rule:nonzero;"></path><path d="M465.457,91.906c-0.789,-1.41 -1.808,-2.673 -3.009,-3.738c-0.676,-0.599 -1.411,-1.132 -2.192,-1.596c0.517,-0.625 0.983,-1.293 1.39,-2c0.021,-0.039 0.043,-0.075 0.062,-0.113c1.165,-2.072 1.829,-4.46 1.829,-7.006c0,-2.515 -0.649,-4.879 -1.788,-6.931c-0.034,-0.063 -0.067,-0.125 -0.103,-0.185c-2.47,-4.297 -7.107,-7.193 -12.42,-7.193l-26.446,0l0,7.378l27.99,-0c3.177,0.705 5.557,3.546 5.557,6.931c0,3.517 -2.567,6.445 -5.926,7.006l-20.428,-0l-0,-10.247l-7.193,-0l0,38.98l26.446,0c0.632,0 1.255,-0.042 1.866,-0.122c0.611,0.08 1.234,0.122 1.866,0.122c5.383,0 10.072,-2.971 12.513,-7.363c1.146,-2.057 1.798,-4.428 1.798,-6.948c0,-2.532 -0.656,-4.911 -1.812,-6.975Zm-11.017,13.923l-24.467,-0l-0,-13.923l24.321,-0c3.28,0.626 5.766,3.515 5.766,6.975c-0,3.409 -2.412,6.265 -5.62,6.948Z" style="fill:#023047;fill-rule:nonzero;"></path></g></svg>
+
+  </div>
+  <div>
+  </div>
+  </div>
+  <h1 class="font-serif text-2xl text-center">Instituto Tecnologico Superior Campus Motul</h1>
+    <div class="p-4"></div>
+        <div class="card mb-5 mx-auto flex w-full gap-4 px-5 py-4 shadow-lg md:w-4/5 ">
+            <div class="w-2/5 md:w-1/3 lg:w-1/4">
+                <img src="https://picsum.photos/250/300" alt="portada del libro" class="mb-3" />
+
+            </div>
+            <div class="md:w-1-3 w-4/5 lg:w-3/4">
+                <h1 class="text-3xl font-bold">¿Qué hacemos en isclab?</h1>
+                <p class="mt-3 text-xl">Desarrollamos aplicaciones con fines de práctica, investigación y responsabilidad social.
+
+Buscamos el desarrollo de nuestros estudiantes a través de la creación de proyectos que sean útiles a la sociedad.
+
+¿Tienes un proyecto en mente? Compártelo con nosotros y buscaremos la manera de ayudarte.</p>
+<div class="p-2"></div>
+                <button class="p-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ">
+                    Me interesa!
+                </button>
+            </div>
+        </div>
+
+    <div class="bg-blue-200"></div>
+    <div class="p-2"></div>
+        <div class="card mb-5 mx-auto flex w-full gap-4 px-5 py-4 shadow-lg md:w-4/5 ">
+          <div class="md:w-1-3 w-4/5 lg:w-3/4">
+                <h1 class="text-3xl font-bold">¿Qué clase de proyectos realizamos?</h1>
+                <p class="mt-3 text-xl">Nuestro laboratorio está pensado para programadores por lo cual nuestros proyectos estareán centrados en ambiente, pero si no sabes programar puedes acudir de igual forma y con el tiempo convertirte en un <strong>master de programación.</strong> </p>
+<div class="p-2"></div>
+                <button class="p-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ">
+                    Me interesa!
+                </button>
+            </div>
+            <div class="w-2/5 md:w-1/3 lg:w-1/4">
+                <img src="https://picsum.photos/250/300" alt="portada del libro" class="mb-3" />
+
+            </div>
+
+        </div>
+      </div>
+
+      <section class="bg-gray-100">
+  <div class="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
+    <div class="items-end justify-between sm:flex">
+      <div class="max-w-xl">
+        <h2 class="text-4xl font-bold tracking-tight sm:text-5xl">
+          Reseñas de nuestros participantes del laboratorio ISCLAB
+        </h2>
+
+      </div>
+
+      <a
+        href="#"
+        class="mt-8 inline-flex flex-shrink-0 items-center rounded-full border border-pink-600 px-5 py-3 font-medium text-pink-600 hover:bg-pink-600 hover:text-white sm:mt-0 lg:mt-8"
+      >
+        Dejar una reseña
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="ml-3 h-4 w-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M14 5l7 7m0 0l-7 7m7-7H3"
+          />
+        </svg>
+      </a>
+    </div>
+
+    <div class="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <blockquote class="flex h-full flex-col justify-between bg-white p-12">
+        <div>
+          <div class="flex gap-0.5 text-green-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+          </div>
+
+          <div class="mt-4">
+            <h3 class="text-xl font-bold text-blue-200 sm:text-2xl">
+              Lorem ipsum dolor sit amet.
+            </h3>
+
+            <p class="mt-4 text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              cumque recusandae dolorum porro, quasi sunt necessitatibus dolorem
+              ab laudantium vel.
+            </p>
+          </div>
+        </div>
+
+        <footer class="mt-8 text-gray-500">Eddie Murphy</footer>
+      </blockquote>
+
+      <blockquote class="flex h-full flex-col justify-between bg-white p-12">
+        <div>
+          <div class="flex gap-0.5 text-green-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+          </div>
+
+          <div class="mt-4">
+            <h3 class="text-xl font-bold text-blue-200 sm:text-2xl">
+              Lorem ipsum dolor sit amet.
+            </h3>
+
+            <p class="mt-4 text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              cumque recusandae dolorum porro, quasi sunt necessitatibus dolorem
+              ab laudantium vel.
+            </p>
+          </div>
+        </div>
+
+        <footer class="mt-8 text-gray-500">Eddie Murphy</footer>
+      </blockquote>
+
+      <blockquote class="flex h-full flex-col justify-between bg-white p-12">
+        <div>
+          <div class="flex gap-0.5 text-green-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              />
+            </svg>
+          </div>
+
+          <div class="mt-4">
+            <h3 class="text-xl font-bold text-blue-200 sm:text-2xl">
+              Lorem ipsum dolor sit amet.
+            </h3>
+
+            <p class="mt-4 text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              cumque recusandae dolorum porro, quasi sunt necessitatibus dolorem
+              ab laudantium vel.
+            </p>
+          </div>
+        </div>
+
+        <footer class="mt-8 text-gray-500">Eddie Murphy</footer>
+      </blockquote>
+
+ </div>
+ </div>
+</section>
+
+</template>
+
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,25 @@
+<script>
+
+<header>
+
+<!-- BARRA DE NAVEGACIÓN  -->
+<div class="text-sm">
+<nav class="bg-black flex justify-between items-center">
+<div class="logo p-2">
+    <img src="logo blanco.png" width="18%" alt="">
+</div>
+<div class="links text-sm md:text-base" >
+    <ul class=" text-sm sm:text-base lg:text-lg menu p-2 sm:p-4 sm:text-center text-white block flex items-center jutify-center gap-5 font-semibold hover:text-gray-700">
+        <li><a href="Principal.html" class="text-white block  ">Home</a></li>
+        <li><a href="Calendario.html" class="text-white block ">Calendario</a></li>
+        <li><a href="mapa.html"class="text-white block">Ubicación</a></li>
+        <li><a href="Registro.html" class=" text-white block ">Registrarse</a></li>
+        <li><a href="Inicio.html" class=" text-white block ">Iniciar sesión</a></li>
+    </ul>
+ </div>
+</nav>
+</div>
+
+
+</header>
+</script>
